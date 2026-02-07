3e0042bb212497b97f8603dd5852285c84167d35
Lucas Carvalho <lucasbuchele@gmail.com>
2026-02-07 16:56:43 -0300

    feat: adding new component - text

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import Text from './components/text';
+
 export default function App() {
-  return <div className="text-green-base"> Hello world! </div>
+  return (
+    <div className="flex flex-col gap-4">
+      <Text variant="body-md-bold"> Levar o dog pra passear </Text>
+      <Text variant="body-md"> Tirar o lixo </Text>
+      <Text variant="body-sm-bold"> Ir ao supermercado </Text>
+    </div>
+  )
 }
