f50b20118e51ed16d86d341520e698d8295dfbdf
Lucas Carvalho <lucasbuchele@gmail.com>
2026-02-08 19:48:07 -0300

    feat: adding new component - button-icon

--- src/App.tsx
+++ src/App.tsx
@@ -8,38 +8,45 @@ import XIcon from './assets/icons/x.svg?react';
 import Icon from './components/icon';
 import Badge from './components/badge';
 import Button from './components/button';
+import ButtonIcon from './components/button-icon';
 
 export default function App() {
-  return (
-    <div className="grid gap-3">
-      <div className="flex flex-col gap-4">
-        <Text variant="body-md-bold"> Levar o dog pra passear </Text>
-        <Text variant="body-md"> Tirar o lixo </Text>
-        <Text variant="body-sm-bold"> Ir ao supermercado </Text>
-      </div>
+    return (
+        <div className="grid gap-3">
+            <div className="flex flex-col gap-4">
+                <Text variant="body-md-bold"> Levar o dog pra passear </Text>
+                <Text variant="body-md"> Tirar o lixo </Text>
+                <Text variant="body-sm-bold"> Ir ao supermercado </Text>
+            </div>
 
-      <div className="flex gap-4">
-        <Icon svg={TrashIcon} />
-        <Icon svg={CheckIcon} />
-        <Icon svg={CircleIcon} />
-        <Icon svg={PlusIcon} />
-        <Icon svg={PencilIcon} />
-        <Icon svg={XIcon} />
-      </div>
+            <div className="flex gap-4">
+                <Icon svg={TrashIcon} />
+                <Icon svg={CheckIcon} />
+                <Icon svg={CircleIcon} />
+                <Icon svg={PlusIcon} />
+                <Icon svg={PencilIcon} />
+                <Icon svg={XIcon} />
+            </div>
 
-      <div className="flex gap-4">
-        <Badge variant="primary" size="sm">
-          <Text variant="body-sm-bold"> 10 </Text>
-        </Badge>
+            <div className="flex gap-4">
+                <Badge variant="primary" size="sm">
+                    <Text variant="body-sm-bold"> 10 </Text>
+                </Badge>
 
-        <Badge variant="secondary" size="sm">
-          <Text variant="body-sm-bold"> 10 </Text>
-        </Badge>
-      </div>
+                <Badge variant="secondary" size="sm">
+                    <Text variant="body-sm-bold"> 10 </Text>
+                </Badge>
+            </div>
 
-      <div className="flex gap-4">
-        <Button icon={PlusIcon}>Nova tarefa</Button>
-      </div>
-    </div>
-  )
+            <div className="flex gap-4">
+                <Button icon={PlusIcon}>Nova tarefa</Button>
+            </div>
+
+            <div className="flex gap-1">
+                <ButtonIcon icon={TrashIcon} disabled />
+                <ButtonIcon icon={TrashIcon} variant="secondary" />
+                <ButtonIcon icon={TrashIcon} variant="tertiary" />
+            </div>
+        </div>
+    )
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,85 +3,85 @@ import Icon from "./icon";
 import Text from "./text";
 
 export const buttonVariants = cva(
-  `
+    `
     flex items-center justify-center cursor-pointer
     transition rounded-lg group gap-2
   `,
-  {
-    variants: {
-      variant: {
-        primary: "bg-gray-200 hover:bg-pink-light",
-      },
-      size: {
-        md: "h-14 py-4 px-5",
-      },
-      disabled: {
-        true: "opacity-50 pointer-events-none",
-      },
-    },
-    defaultVariants: {
-      variant: "primary",
-      size: "md",
-      disabled: false,
-    },
-  }
+    {
+        variants: {
+            variant: {
+                primary: "bg-gray-200 hover:bg-pink-light",
+            },
+            size: {
+                md: "h-14 py-4 px-5",
+            },
+            disabled: {
+                true: "opacity-50 pointer-events-none",
+            },
+        },
+        defaultVariants: {
+            variant: "primary",
+            size: "md",
+            disabled: false,
+        },
+    }
 );
 
 export const buttonIconVariants = cva("transition", {
-  variants: {
-    variant: {
-      primary: "fill-pink-base",
+    variants: {
+        variant: {
+            primary: "fill-pink-base",
+        },
+        size: {
+            md: "w-5 h-5",
+        },
     },
-    size: {
-      md: "w-5 h-5",
+    defaultVariants: {
+        variant: "primary",
+        size: "md",
     },
-  },
-  defaultVariants: {
-    variant: "primary",
-    size: "md",
-  },
 });
 
 export const buttonTextVariants = cva("", {
-  variants: {
-    variant: {
-      primary: "text-gray-400",
+    variants: {
+        variant: {
+            primary: "text-gray-400",
+        },
     },
-  },
-  defaultVariants: {
-    variant: "primary",
-  },
+    defaultVariants: {
+        variant: "primary",
+    },
 });
 
 interface ButtonProps
-  extends Omit<React.ComponentProps<"button">, "size" | "disabled">,
+    extends Omit<React.ComponentProps<"button">, "size" | "disabled">,
     VariantProps<typeof buttonVariants> {
-  icon?: React.ComponentProps<typeof Icon>["svg"];
+    icon?: React.ComponentProps<typeof Icon>["svg"];
 }
 
 export default function Button({
-  variant,
-  size,
-  disabled,
-  className,
-  children,
-  icon: IconComponent,
-  ...props
+    variant,
+    size,
+    disabled,
+    className,
+    children,
+    icon: IconComponent,
+    ...props
 }: ButtonProps) {
-  return (
-    <button
-      className={buttonVariants({ className, disabled, size, variant })}
-      {...props}
-    >
-      {IconComponent && (
-        <Icon
-          svg={IconComponent}
-          className={buttonIconVariants({ variant, size })}
-        />
-      )}
-      <Text variant="body-md-bold" className={buttonTextVariants({ variant })}>
-        {children}
-      </Text>
-    </button>
-  );
+    return (
+        <button
+            className={buttonVariants({ className, disabled, size, variant })}
+            {...props}
+        >
+            {IconComponent && (
+                <Icon
+                    svg={IconComponent}
+                    className={buttonIconVariants({ variant, size })}
+                />
+            )}
+            <Text variant="body-md-bold" className={buttonTextVariants({ variant })}>
+                {children}
+            </Text>
+        </button>
+    );
 }
